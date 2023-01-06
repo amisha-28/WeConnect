36a723b5182c0afb7cb9bbfd8aea782ab69e65b5
RitikaMukherjee <rima.slg1999@gmail.com>
2023-01-06 19:18:21 +0530

    First Commit

---
+++ app/src/main/java/com/example/weconnect/Welcome.java
@@ -0,0 +1,29 @@
+package com.example.weconnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Thread thread=new Thread(){
+            public void run(){
+                try {
+                    sleep(3000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent=new Intent(Welcome.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };thread.start();
+    }
+}
